464da1cc279ef0df952192eec44e16d94aaaf3bc
Leonard Phillips <philll11@users.noreply.github.com>
2018-05-14 21:14:15 +1200

    Structure

---
+++ skeleton/building.py
@@ -0,0 +1,81 @@
+import random
+
+class Supplies:
+    """data object for storing supplies"""
+        
+    def __init__(self, supp):
+        if isinstance(supp, list):
+            self.food = supp[0]
+            self.water = supp[1]
+        else:
+            self.food = supp
+            self.water = supp
+    
+    def __str__(self):
+        if self.food == self.water:
+            return str(self.food)
+        else:
+            return "[{0},{1}]".format(self.food, self.water)
+
+def random_building(size = 10, equal_supplies = True):
+    rooms = []
+    for r in range(2*size+1):
+        row = []
+        for c in range(2*size+1):
+            if equal_supplies:
+                row.append(random.randint(0,9))
+            else:
+                if random.choice([True, False]):
+                    row.append([random.randint(0,9), 0])
+                else:
+                    row.append([0, random.randint(0,9)])
+        rooms.append(row)
+    return Building(rooms)
+
+class Building:
+    """data object for storing a (2n+1) by (2n+1) array of rooms"""
+        
+    def __init__(self, rooms):
+        """rooms = 2D array of integers or pairs of integers"""
+        if len(rooms) % 2 != 1 or len(rooms[0]) != len(rooms):
+            raise ValueError("Illegal number of rooms: must be (2n+1) x (2n+1)")
+        self.size = len(rooms) // 2
+        self.rooms = [[Supplies(s) for s in row] for row in rooms]
+        self.reset()
+        
+    def __str__(self):
+        return "[[" + "],\n [".join( [ ",".join([str(room) for room in row]) for row in self.rooms ] ) + "]]"
+        
+    def reset(self):
+        """player starts in the center, without supplies"""
+        self.player_row = self.size
+        self.player_col = self.size
+        self.player_food = 0
+        self.player_water = 0
+        
+    def is_valid(self, row, col):
+        """checks if (row,col) is a valid room location"""
+        return 0 <= row <= 2*self.size and 0 <= col <= 2*self.size
+        
+    def is_collapsed(self, row, col):
+        """checks if the room at position (row,col) is collapsed"""
+        def is_after(start, x, end):
+            return (start < end and x < end) or (start > end and x > end)
+        return is_after(self.size, row, self.player_row) or is_after(self.size, col, self.player_col)
+        
+    def can_move(self):
+        """check if the player is able to move in any direction"""
+        return not (self.player_row in [0, 2*self.size]) or not (self.player_col in [0, 2*self.size])
+
+    def move_player(self, delta_row, delta_col):
+        """move player by (delta_row, delta_col) vector if possible; returns whether successful"""
+        new_row = self.player_row + delta_row
+        new_col = self.player_col + delta_col
+        if not self.is_valid(new_row, new_col) or self.is_collapsed(new_row, new_col):
+            return False
+        else:
+            self.player_row = new_row
+            self.player_col = new_col
+            self.player_food += self.rooms[self.player_row][self.player_col].food
+            self.player_water += self.rooms[self.player_row][self.player_col].water
+            return True
---
+++ skeleton/collapse.py
@@ -0,0 +1,111 @@
+import building, building_io, optimizer
+import pygame, os, sys, time
+
+# init screen
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+pygame.init() 
+screen_size = [800,600]
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Collapse")
+
+# default config
+size = 1
+equal_supplies = True
+
+# utility functions
+def limit(lower, x, upper):
+    return min(max(lower, x), upper)
+    
+def local_delay(t):
+    """stops the current execution for t seconds without delaying other threads"""
+    start = time.time()
+    while time.time() - start < t: pass
+    
+# command line overwrite
+print("syntax: python collapse.py [size] [easy|hard]")
+if len(sys.argv) > 1 and sys.argv[1].isdigit():
+    size = limit(1, int(sys.argv[1]), 20)
+if len(sys.argv) > 2 and sys.argv[2] == "hard":
+    equal_supplies = False
+
+# drawing functions
+def show_status():
+    myfont = pygame.font.SysFont("Comic Sans MS", 12)
+    def print_at(msg, x, y):
+        label = myfont.render(msg, 1, building_io.white)
+        screen.blit(label, (x,y))
+    if equal_supplies:
+        print_at("Supplies collected:  {0} / {1}".format(my_building.player_food, player_needs), screen_size[1] + 15, 20)
+    else:
+        print_at("Food collected:  {0} / {1}".format(my_building.player_food, player_needs), screen_size[1] + 15, 20)
+        print_at("Water collected:  {0} / {1}".format(my_building.player_water, player_needs), screen_size[1] + 15, 40)
+    print_at("r: reset", screen_size[1] + 15, 60)
+    print_at("n: new", screen_size[1] + 15, 80)
+
+def redraw(flip = True):
+    my_renderer.display_building(screen)
+    show_status()
+    if flip: pygame.display.flip()
+
+# initialization function
+def new_building():
+    global my_building, my_renderer, player_needs
+    my_building = building.random_building(size, equal_supplies)
+    player_needs = optimizer.max_food(my_building) if equal_supplies else optimizer.max_supplies(my_building)
+    # new renderer
+    room_size = screen_size[1] // (2*size + 1)
+    offset = (screen_size[1] - (2*size+1) * room_size) // 2
+    my_renderer = building_io.BuildingRenderer(my_building, (offset,offset), room_size)
+    
+# initialization
+new_building()
+redraw(True)
+
+# -------- Main Program Loop -----------
+while True:
+    event = pygame.event.wait() # get one event, wait until it happens
+    if event.type == pygame.QUIT: # user clicked close
+        break
+    # handle player movement
+    if event.type == pygame.KEYDOWN:
+        if ((event.key == pygame.K_LEFT     and my_building.move_player( 0,-1)) or
+            (event.key == pygame.K_RIGHT    and my_building.move_player( 0, 1)) or
+            (event.key == pygame.K_UP       and my_building.move_player(-1, 0)) or
+            (event.key == pygame.K_DOWN     and my_building.move_player( 1, 0))):
+            # game over?
+            if not my_building.can_move():
+                redraw(False)
+                # show win/loss message
+                myfont = pygame.font.SysFont("Comic Sans MS", 16)
+                supplies = min(my_building.player_food, my_building.player_water)
+                victory = supplies >= player_needs
+                msg = "You collected supplies for {0} out of {1} days. You {2}.".format(supplies, player_needs, "survive" if victory else "die")
+                if victory:
+                    label = myfont.render(msg, 1, building_io.green)
+                else:
+                    label = myfont.render(msg, 1, building_io.red)
+                building_io.center_label(screen, label, screen_size[1]/2, screen_size[1]/2)
+                pygame.display.flip()
+                # progress to next level
+                if victory and (size < 20 or equal_supplies):
+                    local_delay(2.0)
+                    if size < 20:
+                        size += 1
+                    else:
+                        size = 1
+                        equal_supplies = False
+                    new_building()
+                    redraw()
+            else:
+                redraw()
+        elif event.key == pygame.K_p: # print current game to stdout
+            print(my_building)
+        elif event.key == pygame.K_n: # new game
+            new_building()
+            redraw()
+        elif event.key == pygame.K_r: # reset current game
+            my_building.reset()
+            redraw()
+     
+# clean exit
+pygame.quit()
