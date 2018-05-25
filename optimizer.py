bc1f7a6312d060c30a2a30c7bb69dedf9db5c5a6
Leonard Phillips <philll11@users.noreply.github.com>
2018-05-25 22:06:23 +1200

    Working algorithm
    Completes in 24 seconds. Not fast enough. Needs more pruning steps

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,5 @@
 from building import *
+from collections import OrderedDict
 
 # speed test - use "python optimizer.py" to run
 if __name__ == "__main__":
@@ -14,33 +15,36 @@ def max_food(building):
     """returns the maximum number of food that can be collected from given building"""
     start = building.size
     length = (building.size * 2) + 1
-    matrix = [[0 for x in range(length)] for x in range(length)]
+    matrix = [[building.rooms[y][x].food for x in range(length)] for y in range(length)]
+    # matrix = [[0 for x in range(length)] for x in range(length)]
 
     # Initialize started points for the four quadrants
-    matrix[start - 1][start] = building.rooms[start - 1][start].food
-    matrix[start + 1][start] = building.rooms[start + 1][start].food
-    matrix[start][start - 1] = building.rooms[start][start - 1].food
-    matrix[start][start + 1] = building.rooms[start][start + 1].food
+    # matrix[start - 1][start] = building.rooms[start - 1][start].food
+    # matrix[start + 1][start] = building.rooms[start + 1][start].food
+    # matrix[start][start - 1] = building.rooms[start][start - 1].food
+    # matrix[start][start + 1] = building.rooms[start][start + 1].food
+
+    matrix[start][start] = 0
 
     # Initialize columns for upper and lower quadrants
     i = start - 1
     while i > 0:
-        matrix[i - 1][start] = building.rooms[i - 1][start].food + matrix[i][start]
+        matrix[i - 1][start] = matrix[i - 1][start] + matrix[i][start]
         i = i - 1
     i = start + 1
     while i < length - 1:
-        matrix[i + 1][start] = building.rooms[i + 1][start].food + matrix[i][start]
+        matrix[i + 1][start] = matrix[i + 1][start] + matrix[i][start]
         i = i + 1
 
     # Initialize rows for left and right quadrants
-    j = start - 1
-    while j > 0:
-        matrix[start][j - 1] = building.rooms[start][j - 1].food + matrix[start][j]
-        j = j - 1
     j = start + 1
     while j < length - 1:
-        matrix[start][j + 1] = building.rooms[start][j + 1].food + matrix[start][j]
+        matrix[start][j + 1] = matrix[start][j + 1] + matrix[start][j]
         j = j + 1
+    j = start - 1
+    while j > 0:
+        matrix[start][j - 1] = matrix[start][j - 1] + matrix[start][j]
+        j = j - 1
 
 
     # Builds upper left matrix
@@ -48,7 +52,7 @@ def max_food(building):
     while i >= 0:
         j = start - 1
         while j >= 0:
-            matrix[i][j] = max(matrix[i + 1][j], matrix[i][j + 1]) + building.rooms[i][j].food
+            matrix[i][j] = max(matrix[i + 1][j], matrix[i][j + 1]) + matrix[i][j]
             j = j - 1
         i = i - 1
 
@@ -57,7 +61,7 @@ def max_food(building):
     while i >= 0:
         j = start + 1
         while j < length:
-            matrix[i][j] = max(matrix[i + 1][j], matrix[i][j - 1]) + building.rooms[i][j].food
+            matrix[i][j] = max(matrix[i + 1][j], matrix[i][j - 1]) + matrix[i][j]
             j = j + 1
         i = i - 1
 
@@ -66,12 +70,12 @@ def max_food(building):
     while i < length:
         j = start + 1
         while j < length:
-            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]) + building.rooms[i][j].food
+            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]) + matrix[i][j]
             j = j + 1
         i = i + 1
 
     # Builds lower left matrix
-    i = start - 1
+    i = start + 1
     while i < length:
         j = start - 1
         while j >= 0:
@@ -79,17 +83,225 @@ def max_food(building):
             j = j - 1
         i = i + 1
 
+    # for i in range(0, length):
+    #     for j in range(0, length):
+    #         print(building.rooms[i][j], end="\t")
+    #     print()
+    # print("\n\n\n")
+
+    #print(max(matrix[0][0], matrix[0][length - 1], matrix[length - 1][0], matrix[length - 1][length - 1]))
+
+    return max(matrix[0][0], matrix[0][length - 1], matrix[length - 1][0], matrix[length - 1][length - 1])
+
+def addToDict(currentDict, previousDict):
+    for key in previousDict:
+        if key in currentDict:
+            currentDict[key].append(previousDict[key])
+        else:
+            currentDict[key] = [previousDict[key]]
+
+
+def max_supplies(building):
+    start = building.size
+    length = (building.size * 2) + 1
+    # Building the matrix
+    matrix = [[{} for x in range(length)] for y in range(length)]
+
+    # Player start point
+    matrix[start][start] = {0: [0]}
+
+    # Initialize started points for the four quadrants
+    matrix[start - 1][start] = {building.rooms[start - 1][start].food: [building.rooms[start - 1][start].water]}
+    matrix[start + 1][start] = {building.rooms[start + 1][start].food: [building.rooms[start + 1][start].water]}
+    matrix[start][start - 1] = {building.rooms[start][start - 1].food: [building.rooms[start][start - 1].water]}
+    matrix[start][start + 1] = {building.rooms[start][start + 1].food: [building.rooms[start][start + 1].water]}
+
+    # Initialize columns for upper and lower quadrants
+    i = start - 1
+    while i > 0:
+        matrix[i - 1][start] = {building.rooms[i - 1][start].food + list(matrix[i][start].keys())[0]:
+                                    [building.rooms[i - 1][start].water + list(matrix[i][start].values())[0][0]]}
+        i = i - 1
+    i = start + 1
+    while i < length - 1:
+        matrix[i + 1][start] = {building.rooms[i + 1][start].food + list(matrix[i][start].keys())[0]:
+                                    [building.rooms[i + 1][start].water + list(matrix[i][start].values())[0][0]]}
+        i = i + 1
+
+    # Initialize rows for left and right quadrants
+    j = start - 1
+    while j > 0:
+        matrix[start][j - 1] = {building.rooms[start][j - 1].food + list(matrix[start][j].keys())[0]:
+                                    [building.rooms[start][j - 1].water + list(matrix[start][j].values())[0][0]]}
+        j = j - 1
+    j = start + 1
+    while j < length - 1:
+        matrix[start][j + 1] = {building.rooms[start][j + 1].food + list(matrix[start][j].keys())[0]:
+                                    [building.rooms[start][j + 1].water + list(matrix[start][j].values())[0][0]]}
+        j = j + 1
+
+    # Builds upper left matrix
+    i = start - 1
+    while i >= 0:
+        j = start - 1
+        while j >= 0:
+
+            # Adding water from cell below
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i + 1][j]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i + 1][j][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            # Adding food from neighbouring cell
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i][j + 1]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i][j + 1][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            j = j - 1
+        i = i - 1
+
+
+    # Builds upper right matrix
+    i = start - 1
+    while i >= 0:
+        j = start + 1
+        while j < length:
+
+            # Adding water from cell below
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i + 1][j]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i + 1][j][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            # Adding food from neighbouring cell
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i][j - 1]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i][j - 1][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            j = j + 1
+        i = i - 1
+
+    # Builds lower right matrix
+    i = start + 1
+    while i < length:
+        j = start + 1
+        while j < length:
+
+            # Adding water from cell below
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i - 1][j]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i - 1][j][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            # Adding food from neighbouring cell
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i][j - 1]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i][j - 1][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            j = j + 1
+        i = i + 1
+
+    # Builds lower right matrix
+    i = start + 1
+    while i < length:
+        j = start - 1
+        while j >= 0:
+
+            # Adding water from cell below
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i - 1][j]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i - 1][j][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            # Adding food from neighbouring cell
+            roomKey = building.rooms[i][j].food
+            roomValue = building.rooms[i][j].water
+            for matrixKey in matrix[i][j + 1]:
+                keySum = roomKey + matrixKey
+                listOfValues = matrix[i][j + 1][matrixKey]
+                if len(listOfValues) > 1:
+                    listOfValues.sort()
+                valueSum = roomValue + listOfValues[0]
+
+                if keySum in matrix[i][j]:
+                    matrix[i][j][keySum].append(valueSum)
+                else:
+                    matrix[i][j][keySum] = [valueSum]
+
+            j = j - 1
+        i = i + 1
 
     # for i in range(0, length-1):
     #     for j in range(0, length-1):
     #         print(matrix[i][j], end="\t")
     #     print()
-    # print("\n\n\n")
-    #print(max(matrix[0][0], matrix[0][length - 1], matrix[length - 1][0], matrix[length - 1][length - 1]))
+    # print("\n\n")
 
-    return max(matrix[0][0], matrix[0][length - 1], matrix[length - 1][0], matrix[length - 1][length - 1])
-
-
-def max_supplies(building):
-    """returns the maximum of min(food,water) that can be collected from given building"""
-    return building.size * 5 # dummy implementation - replace
+    list1 = [matrix[0][0], matrix[0][length - 1], matrix[length - 1][0], matrix[length - 1][length - 1]]
+    return list1
